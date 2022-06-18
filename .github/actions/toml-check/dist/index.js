3ce2cd4667f09256e3e8bb45270d11bd4126ca23
Benson Liu <bensonhliu@gmail.com>
2022-06-17 23:58:21 -0400

    updated dist

--- .github/actions/toml-check/dist/index.js
+++ .github/actions/toml-check/dist/index.js
@@ -18,7 +18,7 @@ var schema;
 try {
   schema = loadTomlFile(withGithubWorkspacePath(core.getInput('path-to-schema')));
 } catch (e) {
-  console.log('ERROR: Schema could not be loaded')
+  console.log('ERROR: Challenge Schema could not be loaded')
   core.setFailed('Check output for errors')
   exit(1)
 }
@@ -39,6 +39,91 @@ fs.readdir(challengesPath, (err, folders) => {
     console.log('CHALLENGE: ' + folder)
 
     const filepath = `${challengesPath}/${folder}/challenge.toml`;
+    try {
+      const data = loadTomlFile(filepath);
+      
+      // validate
+      Object.keys(schema).forEach(key => {
+        // validate fields are present
+        if (!(key in data)) {
+          status = false;
+          console.log(`ERROR: In ${folder}: ${key} field is missing`)
+        } else {
+          // validate type
+          switch (schema[key]) {
+            case "array":
+              if (!Array.isArray(data[key])) {
+                status = false;
+                console.log(`ERROR: In ${folder}: ${key} field is of the wrong type. It should be type array`)
+              }
+              break;
+            default:
+              if (typeof data[key] !== schema[key]) {
+                status = false;
+                console.log(`ERROR: In ${folder}: ${key} field is of the wrong type. It should be type ${schema[key]}`)
+              }
+          }
+        }
+
+        // special validation
+        if (key === 'files' && Array.isArray(data[key])) {
+          data[key].forEach(file => {
+            if ('description' in data && typeof data['description'] === "string") {
+              if (data['description'].search(new RegExp(`\\[.+\\]\\(${file}\\)`)) === -1) {
+                status = false;
+                console.log(`ERROR: In ${folder}: Description is missing link to ${file}`)
+              }
+
+              const matches = data['description'].match(/\[.+\]\(([-a-zA-Z0-9()_.]+)\)/);
+              
+              matches.forEach((val, i) => {
+                if (i % 2 === 1) {
+                  if (!data['files'].includes(val)) {
+                    status = false;
+                    console.log(`ERROR: In ${folder}: ${val} is missing in files`)
+                  }
+                }
+              })
+            }
+          })
+        }
+      })
+    } catch (e) {
+      console.log(`ERROR: In ${folder} toml file on line ${e.line}, column ${e.column}: ${e.message}`);
+      status = false;
+    }
+  });
+
+  if (!status) {
+    core.setFailed('Check output for errors')
+  }
+})
+
+var eventSchema; 
+try {
+  eventSchema = loadTomlFile(withGithubWorkspacePath(core.getInput('path-to-schema-events')));
+} catch (e) {
+  console.log('ERROR: Event Schema could not be loaded')
+  core.setFailed('Check output for errors')
+  exit(1)
+}
+
+const eventsPath = withGithubWorkspacePath('events/');
+
+fs.readdir(eventsPath, (err, folders) => {
+  if (err) {
+    console.log('ERROR: Could not open events/')
+    core.setFailed('Check output for errors')
+    return
+  }
+
+  let status = true;
+  
+  folders.filter(folder => folder !== "README.md").forEach(folder => {
+    console.log('______________________________________________________')
+    console.log('EVENT: ' + folder)
+
+    const filepath = `${challengesPath}/${folder}/event.toml`;
     try {
       const data = loadTomlFile(filepath);
       
