320cf9d6225143723c4f1a1e2d31583143342c22
Jerry Xu <54759887+xu-jerry@users.noreply.github.com>
2022-12-29 15:24:56 -0800

    Revert "test"
    This reverts commit e88476cde9ab776a5dd23c49a83725d9daa7332f.

--- .github/actions/toml-check/dist/index.js
+++ .github/actions/toml-check/dist/index.js
@@ -154,14 +154,6 @@ fs.readdir(eventsPath, (err, folders) => {
       console.log(`ERROR: In ${folder} toml file on line ${e.line}, column ${e.column}: ${e.message}`);
       status = false;
     }
-
-    var iconPath = `${eventsPath}/${folder}/icon.svg`;
-    console.log(!fs.existsSync(iconPath));
-    if (!fs.existsSync(iconPath)) {
-      console.log(`ERROR: In ${folder}: icon.svg is missing`);
-      status = false;
-      return;
-    }
   });
 
   if (!status) {
